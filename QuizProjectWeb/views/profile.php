0e0108c2c6678c5bd9f216a0dfa996382d77331e
AlpEvan <evan.alpst@eduge.ch>
2024-12-20 16:06:58 +0100

    Add files via upload

--- QuizProjectWeb/views/profile.php
+++ QuizProjectWeb/views/profile.php
@@ -69,6 +69,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     <input type="submit" value="Confirmer les changements">
                 </form>
+    <br>
+                <a href="../index.php"><button>Retour</button></a>
             </div>
             
             <div class="card-back">
